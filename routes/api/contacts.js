bcd590823845f7a12811c28f79ab91c3ff9f98a1
Nataliia_Rud <nrud1981@gmail.com>
2023-06-26 16:41:26 +0300

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,23 +1,20 @@
 const express = require("express");
-// const {
-//   getById,
-//   add,
-//   deleteById,
-//   updateById,
-// } = require("../../controllers/contacts");
 
 const router = express.Router();
+
 const ctrl = require("../../controllers/contacts");
+
+const { validateBody } = require("../../middlewares");
 const schemas = require("../../schemas/contacts");
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getById);
 
-router.post("/", ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
+
+router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
 
 router.delete("/:contactId", ctrl.deleteById);
-
-router.put("/:contactId", ctrl.updateById);
 
 module.exports = router;
