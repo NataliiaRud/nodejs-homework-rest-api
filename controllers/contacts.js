9cbd1c849b2e4fa7c4934cd571a110d7d53adfa0
Nataliia_Rud <nrud1981@gmail.com>
2023-07-02 12:43:44 +0300

    update

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -16,7 +16,7 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await contacts.add(req.body);
+  const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
 
