06db08208b4550b2cd394579d2fa64f35278a7a3
NataliiaRud <nrud1981@gmail.com>
2023-07-07 15:44:19 +0300

    update

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const app = require('./app')
 
-const { DB_HOST } = process.env;
+const  DB_HOST  = 'mongodb+srv://Natalie:ldjhHXcjpPGuFC8v@cluster0.3crhwa0.mongodb.net/db-contacts?retryWrites=true&w=majority'
 
 mongoose.set("strictQuery", true);
 mongoose.connect(DB_HOST)
 .then(() => {
-  console.log("Database connection successful");
-  app.listen(3000);
   
+  app.listen(3000);
+  console.log("Database connection successful");
 })
 .catch((error) => {console.log(error.message);
 process.exit(1)
